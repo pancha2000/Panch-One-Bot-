5cc399f1a6366ca046c30a1ca4a4f2e4c3edcad5
pancha2000 <119848240+pancha2000@users.noreply.github.com>
2025-01-13 08:59:08 +0530

    index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `Wa-BOT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://mega.nz/file/XuZCQDLY#_zEwkrPMYuZkd80FnIEweCfQWiaCDYRiZPxdGNgLPh0` }, caption: up })
 
 }
 })
