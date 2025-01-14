9e80776f959d2b238f90bd01f8a9da7880ddadf8
pancha2000 <119848240+pancha2000@users.noreply.github.com>
2025-01-14 21:10:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "3zAiXbDR#ZGqyfjNslSy6D1OATudvnDsgi5CMVHMO3KieA8NNSKk",
-MONGODB: process.env.MONGODB || "  ",
+MONGODB: process.env.MONGODB || "mongodb+srv://panchareal486:2006.Shehan@cluster0w.orvet.mongodb.net/",
 };
